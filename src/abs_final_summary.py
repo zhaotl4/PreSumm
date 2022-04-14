ac35af763c0b5f40f81b658d8ff85941a41c0074
zhaotl4 <1780977456@qq.com>
2022-04-14 18:26:35 +0800

    fix abs path

--- src/abs_final_summary.py
+++ src/abs_final_summary.py
@@ -1,6 +1,6 @@
 def abs_final_summ():
     ans = ""
-    file_path = '../logs/abs_bert_cnndm.-1.candidate'
+    file_path = '/home/ztl/nlp/PreSumm/logs/abs_bert_cnndm.-1.candidate'
     with open(file_path,'r') as f:
         lines=f.readlines()
         for line in lines:
